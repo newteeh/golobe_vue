5926b4ef713477ad5d9493cbc262399bd8b97b5f
newteeh <danya.pavlov.54@mail.ru>
2023-05-24 08:35:20 +0500

    Created and modified header

---
+++ src/components/customSelect.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="custom-select">
+    <div class="selected-item" @click="toggleDropdown">
+      {{ selectedItem || placeholder }}
+      <i class="arrow-down"></i>
+    </div>
+    <ul v-if="isOpen" class="dropdown-list">
+      <li v-for="option in options" :key="option" @click="selectOption(option)">
+        {{ option }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "customSelect",
+  data() {
+    return {
+      isOpen: false,
+      selectedItem: '',
+      options: ['Option 1', 'Option 2', 'Option 3'], // Замените на свои опции
+      placeholder: 'Select an option', // Замените на свой заголовок по умолчанию
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isOpen = !this.isOpen;
+    },
+    selectOption(option) {
+      this.selectedItem = option;
+      this.isOpen = false;
+    },
+  },
+}
+</script>
+
+<style scoped>
+.custom-select {
+  position: relative;
+  display: inline-block;
+}
+
+.selected-item {
+  padding: 8px 16px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+
+.arrow-down {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+}
+
+.dropdown-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  border: 1px solid #ccc;
+  border-top: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+}
+
+.dropdown-list li {
+  padding: 8px 16px;
+  cursor: pointer;
+}
+
+.dropdown-list li:hover {
+  background-color: #f2f2f2;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,92 @@
+
+<template>
+  <div class="wrapper">
+    <div class="main-container">
+      <div class="main-page__header">
+        <div class="main-page__header__body">
+          <div class="main-page__header__body__left">
+            <div>
+              <img src="@/assets/icons/airplane.png"/>
+              <a href="" class="">Find Flight</a>
+            </div>
+            <div>
+              <img src="@/assets/icons/ion_bed.png"/>
+              <a href="" class="">Find Stays</a>
+            </div>
+          </div>
+          <div class="main-page__header__body__logo"><img src="@/assets/img/Logo.png"/></div>
+          <div class="main-page__header__body__right">
+            <a href="" class="login__link">Login</a>
+            <a href="" class="signup__link">Sign up</a>
+          </div>
+        </div>
+        <div class="main-page__title-box">
+          <p class="main-page__title-box__headline">Helping Others</p>
+          <h1 class="main-page__title-box__title">Live & Travel</h1>
+          <p class="main-page__title-box__subtitle">Special offers to suit your plan</p>
+        </div>
+
+      </div>
+      <div class="main-page__search-menu">
+        <div class="small-container">
+          <div class="main-page__search-menu__body">
+            <div class="main-page__search-menu__body__form">
+              <div class="main-page__search-menu__body__form__up">
+                <div class="main-page__search-menu__body__form__up__flights-box">
+                  <label for="flights">
+                    <img src="@/assets/icons/airplane__black.png" alt="">
+                    <p>Flights</p>
+                  </label>
+                  <input style="opacity: 0" type="radio" id="flights" name="search__category">
+                </div>
+                <div class="main-page__search-menu__body__form__up__line">
+                  <img src="@/assets/icons/Line 1.png" alt="">
+                </div>
+                <div class="main-page__search-menu__body__form__up__stays-box">
+                  <label for="stays">
+                    <img src="@/assets/icons/ion_bed__black.png" alt="">
+                    <p>Stays</p>
+                  </label>
+                  <input style="opacity: 0;" type="radio" id="stays" name="search__category">
+                </div>
+              </div>
+              <div class="main-page__search-menu__body__form__middle">
+                <div class="select-box__from-to">
+                  <div class="select-box__from">
+                    <label for="from">From</label>
+                    <select name="from" id="from">
+                      <option value="1">Lahore</option>
+                      <option value="2"></option>
+                      <option value="3"></option>
+                    </select>
+                  </div>
+                  <div class="select-box__to">
+                    <label for="to">To</label>
+                    <select name="to" id="to">
+                      <option value="1">Lahore</option>
+                      <option value="2"></option>
+                      <option value="3"></option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name:"Home"
+  }
+</script>
+
+
+
+<style scoped>
+
+</style>
